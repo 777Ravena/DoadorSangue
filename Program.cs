0c4884f99ed83e45a04812940c4f6d59ba352f49
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-29 22:39:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+ 
+int idade;
+ 
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Qual é a sua idade:");
+Console.ResetColor();
+ 
+idade = Convert.ToInt32(Console.ReadLine());
+ 
+if (idade >= 18 && idade <= 67)
+{
+    Console.ForegroundColor = ConsoleColor.DarkYellow;
+    Console.WriteLine("Você pode ser doador de sangue!");
+    Console.ResetColor();
+}
+ 
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+    Console.WriteLine("Você não pode ser doador de Sangue");
+    Console.ResetColor();
+}
